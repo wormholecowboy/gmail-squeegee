b8610eab5262b60c7630958d2e3a5a00cb8bd0c7
wormholecowboy <wormholecowboy@gmail.com>
2023-06-10 13:18:45 -0400

    init

--- gmail-squeegee.js
+++ gmail-squeegee.js
@@ -1,1 +1,27 @@
+import 'google-apps-script';
 
+let ss = SpreadsheetApp.getActiveSpreadsheet()
+let unsubSheet = ss.getSheetByName('Unsubscribe')
+
+
+
+function onOpen() {
+    SpreadsheetApp.getUi().createMenu('Gmail Squeegee')
+        .addItem('Start Trigger', 'createTrigger')
+        .addItem('Run Manual', 'autoArchive')
+        .addToUi()
+    showUnsubscribe()
+}
+
+function createTrigger() {
+    ScriptApp.newTrigger('autoArchive')
+        .timeBased()
+        .everyDays(1)
+        .atHour(23)
+        .create()
+}
+
+function showUnsubscribe() {
+    hasUnsubscribe = GmailApp.search('in:inbox label:' + 'unsubscribe')
+
+}
